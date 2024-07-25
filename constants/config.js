9cda0a7b70c99748f029dc22ed99acbacc9829bf
geetika1312 <121809849+geetika1312@users.noreply.github.com>
2024-07-26 03:03:07 +0530

    PUSH

--- constants/config.js
+++ constants/config.js
@@ -3,6 +3,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:4173",
     process.env.CLIENT_URL,
+    "https://chatapp-frontend-muvl.vercel.app", // Add your frontend URL here
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
